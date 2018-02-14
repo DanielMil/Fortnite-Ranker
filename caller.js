b66826c3a4b48ae29fca3c5d5008534d3cb106d6
Kia Shakiba <k2shakib@uwaterloo.ca>
2018-02-14 17:46:45 -0500

    Fix caller file

--- caller.js
+++ caller.js
@@ -1,15 +1,15 @@
 window.onload = function () {
+	var call = new XMLHttpRequest();
 
 	call.onreadystatechange = function() {
 		if (call.readyState == 4) {
 			if (call.status == 200) {
-				var data = http.response; 
-				console.log(data); 
+				var data = http.response;
+				console.log(data);
 			}
 		}
 	}
 
-	var call = new XMLHttpRequest();
 	call.open("GET", "localhost:3400/endpoint?arg1=OnlyLukey&arg2=OnlyLukey", true);
 	call.send();
 }
