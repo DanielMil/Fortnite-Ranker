189570b37e87f1ec05682cdcd0d7f6125cc71b36
DanielMilGuelph <dmil@mail.uoguelph.ca>
2018-02-13 23:45:16 -0500

    Add files via upload

--- testAPI.js
+++ testAPI.js
@@ -1,17 +1,23 @@
-window.onload = function () {
+// window.onload = function () {
 
-	var http = new XMLHttpRequest();
+// 	var http = new XMLHttpRequest();
 
-	http.onreadystatechange = function() {
-		if (http.readyState == 4) {
-			if (http.status == 200) {
-				var data = JSON.parse(http.response);
-				console.log(data);
-			}
-		}
-	}
+// 	http.onreadystatechange = function() {
+// 		if (http.readyState == 4) {
+// 			if (http.status == 200) {
+// 				var data = JSON.parse(http.response);
+// 				console.log(data);
+// 			}
+// 		}
+// 	}
 
-	http.open("GET", "https://api.fortnitetracker.com/v1/profile/psn/OnlyLukey", true);
-	http.setRequestHeader("TRN-Api-Key", "14116965-82c4-4733-bb71-bc4f6ea9f9d1");
-	http.send();
-}
+// 	http.open("GET", "https://api.fortnitetracker.com/v1/profile/psn/OnlyLukey", true);
+// 	http.setRequestHeader("TRN-Api-Key", "14116965-82c4-4733-bb71-bc4f6ea9f9d1");
+// 	http.send();
+// }
+
+const Fortnite = require('fortnite');
+const client = new Fortnite('14116965-82c4-4733-bb71-bc4f6ea9f9d1');
+var prom = client.getInfo('TheOnlyPrime', 'psn').then(data => {
+	console.log(data.lifetimeStats[6].value); 
+});
